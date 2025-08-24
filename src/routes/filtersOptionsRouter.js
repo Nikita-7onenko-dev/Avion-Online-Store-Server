9654b404fea58d2853d84115b08efdd57b950c44
Nikita-7onenko-dev <nikitatrihomkin@gmail.com>
2025-08-24 07:20:12 +0300

    fix letter case

--- src/routes/filtersOptionsRouter.js
+++ src/routes/filtersOptionsRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import filtersOptionsController from "../controllers/filtersOptionsController.js";
+import filtersOptionsController from "../controllers/FiltersOptionsController.js";
 
 
 const router = express.Router();
