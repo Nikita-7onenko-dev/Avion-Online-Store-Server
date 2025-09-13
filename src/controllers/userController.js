6a8db7d4d760c9e3a8459fbc36966cb430c364f8
Nikita-7onenko-dev <nikitatrihomkin@gmail.com>
2025-09-13 14:00:26 +0300

    change redirect link in activate endpoint

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -32,7 +32,7 @@ class UserController{
     try{
       const activationLink = req.params.link;
       await userService.activate(activationLink);
-      return res.redirect(`${process.env.CLIENT_URL}profile`);
+      return res.redirect(`${process.env.CLIENT_URL}#/profile`);
     } catch(err) {
       handleMongoDBError(err);
     }
